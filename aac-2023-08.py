812e4d152d58321c5d00090cd904a7f17bbc6894
moiv3 <159528029+moiv3@users.noreply.github.com>
2024-11-17 15:01:00 +0800

    day 8 part 2 completed

--- aac-2023-08.py
+++ aac-2023-08.py
@@ -1,8 +1,15 @@
 import argparse
+import math
+from functools import reduce
 
 def read_list(file_path):
     with open(file_path, "r") as fp:
         return fp.read().splitlines()
+
+
+def lcm_of_list(numbers):
+    return reduce(math.lcm, numbers)
+
 
 def get_tree(lst):
     steps = lst[0]
@@ -11,7 +18,8 @@ def get_tree(lst):
         result_dict[item[0:3]] = (item[7:10], item[12:15])
     return steps, result_dict
 
-def traverse_tree(steps, input_dict):
+
+def traverse_tree_part_one(steps, input_dict):
     start_node = 'AAA'
     end_node = 'ZZZ'
     step_len = len(steps)
@@ -26,8 +34,41 @@ def traverse_tree(steps, input_dict):
             steps_travelled += 1
         else:
             raise Exception("Something strange happened!")
-    print(start_node)
     return steps_travelled
+
+
+def traverse_tree_part_two(steps, input_dict):
+    a_list = []
+    for item in input_dict:
+        if item[2] == 'A':
+            a_list.append(item)
+
+    a_steps_list = []
+
+    step_len = len(steps)
+
+    for item in a_list:
+        print(item)
+        steps_travelled = 0
+        while steps_travelled <= 1000000:
+            direction = steps[steps_travelled % step_len]
+            if direction == 'L':
+                item = input_dict[item][0]
+                steps_travelled += 1
+            elif direction == 'R':
+                item = input_dict[item][1]
+                steps_travelled += 1
+            else:
+                raise Exception("Something strange happened!")
+            
+            if item[2] == 'Z':
+                print(item, steps_travelled)
+                a_steps_list.append(steps_travelled)
+                break
+
+    result = lcm_of_list(a_steps_list)
+    return result
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("D")
@@ -36,6 +77,7 @@ if __name__ == "__main__":
     input_list = read_list(args.file)
     # print(input_list)
     steps, input_dict = get_tree(input_list)
-    steps_travelled = traverse_tree(steps, input_dict)
-    print(steps, len(steps))
-    print(steps_travelled)
+    steps_travelled_1 = traverse_tree_part_one(steps, input_dict)
+    steps_travelled_2 = traverse_tree_part_two(steps, input_dict)
+    print(steps_travelled_1)
+    print(steps_travelled_2)
