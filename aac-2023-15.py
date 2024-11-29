a582064bcc71bab0c0d86840866ececfdf95d6bf
moiv3 <159528029+moiv3@users.noreply.github.com>
2024-11-29 23:52:20 +0800

    day 15 part 2 finished

--- aac-2023-15.py
+++ aac-2023-15.py
@@ -1,5 +1,5 @@
 import argparse
-from time import perf_counter
+import pprint
 
 def read_list(filepath):
     with open(filepath, "r") as fp:
@@ -19,7 +19,6 @@ def hash_string(string: str) -> int:
         init_value = (init_value * 17) % 256
     return init_value
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser("AAC-2023-Day13")
     parser.add_argument("file", help="path to the txt file")
@@ -29,7 +28,55 @@ if __name__ == "__main__":
 
     result = []
     for item in input_list:
-        subresult = hash_string(item)
-        result.append(subresult)
+        item_steps = {}
+        if "-" in item:
+            item_steps["lens"] = item.split("-")[0]
+            item_steps["motion"] = "-"
+            item_steps["focallength"] = None
+            item_steps["hash"] = hash_string(item.split("-")[0])
+        elif "=" in item:
+            item_steps["lens"] = item.split("=")[0]
+            item_steps["motion"] = "="
+            item_steps["focallength"] = item.split("=")[1]
+            item_steps["hash"] = hash_string(item.split("=")[0])
+        result.append(item_steps)
+    pprint.pprint(result)
+
+    hashmap = {}
+    for step in result:
+        if step["motion"] == "-":
+            if step["hash"] in hashmap:
+                for d in hashmap[step["hash"]]:
+                    # print(d.keys()[0])
+                    if [*d][0] == step["lens"]:
+                        hashmap[step["hash"]].remove(d)
+        elif step["motion"] == "=":
+            # print("========", hashmap)
+            # print("==========", step["lens"])
+            if step["hash"] not in hashmap or step["hash"] == []:
+                hashmap[step["hash"]] = [{step["lens"]: int(step["focallength"])}]
+
+            elif step["hash"] in hashmap:
+                flag = True
+                for d in hashmap[step["hash"]]:
+                    if [*d][0] == step["lens"]:
+                        flag = False
+                        print("focallength substituted")
+                        d[step["lens"]] = int(step["focallength"])
+                if flag is True:
+                    hashmap[step["hash"]].append({step["lens"]:int(step["focallength"])})
+            else:
+                raise Exception("Exception B")
+        else:
+            raise Exception("Exception A: motion is not - or =")
+        # print(hashmap)
+    # pprint.pprint(hashmap)
+
+    result = 0
+    for i in hashmap:
+        if hashmap[i]:
+            for idx, item in enumerate(hashmap[i]):
+                key = list(item.keys())[0]  # Get the single key in the dictionary
+                value = item[key]
+                result += (i + 1) * (idx + 1) * value
     print(result)
-    print(sum(result))
