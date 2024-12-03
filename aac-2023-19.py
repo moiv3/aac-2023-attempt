339069f1f47fc802dc233a3b19bc5bf49f64f15a
moiv3 <159528029+moiv3@users.noreply.github.com>
2024-12-04 07:24:07 +0800

    day 19 part 2 completed

--- aac-2023-19.py
+++ aac-2023-19.py
@@ -8,7 +8,7 @@ def read_list(filepath):
     
 def do_step(metal_dict, manual_note):
     print(metal_dict, manual_note)
-    for idx, step_detail in enumerate(manual_dict[manual_note]):
+    for step_detail in manual_dict[manual_note]:
         # last step
         if ":" not in step_detail and step_detail == "A":
             accept_bin.append(metal_dict)
@@ -35,12 +35,72 @@ def do_step(metal_dict, manual_note):
                     accept_bin.append(metal_dict)
                     print("==Part accepted==")
                     return
-                if next_step == "R":
+                elif next_step == "R":
                     reject_bin.append(metal_dict)                    
                     print("==Part rejected==")
                     return
                 else:
                     return do_step(metal_dict, next_step)
+            else:
+                continue
+
+def do_step_new(metal_dict, manual_note):
+    print(metal_dict, manual_note)
+    for step_detail in manual_dict[manual_note]:
+        # last step
+        if ":" not in step_detail and step_detail == "A":
+            accept_bin.append(metal_dict)
+            print("==Remaining parts accepted==")
+            return
+        elif ":" not in step_detail and step_detail == "R":
+            reject_bin.append(metal_dict)                   
+            print("==Remaining parts rejected==")
+            return
+        elif ":" not in step_detail:
+            return do_step_new(metal_dict, step_detail)
+        
+        # not last step
+        else:
+            next_step = step_detail.split(":")[1]
+            remain = step_detail.split(":")[0]
+            xmas = step_detail[0]
+            sign = step_detail[1]
+            target = remain[2:]
+            print(next_step, remain, xmas, sign, target)
+            # match conditions
+            if (sign == ">" and metal_dict[xmas][1] > int(target)):
+            
+            # or (sign == "<" and metal_dict[xmas] < int(target)):
+                metal_dict_temp = metal_dict.copy()
+                metal_dict_temp[xmas] = (int(target)+1, metal_dict[xmas][1])
+                metal_dict[xmas] = (metal_dict[xmas][0], int(target))
+                if next_step == "A":
+                    accept_bin.append(metal_dict_temp)
+                    print("==Part accepted==")
+                    continue
+                elif next_step == "R":
+                    reject_bin.append(metal_dict_temp)                    
+                    print("==Part rejected==")
+                    continue
+                else:
+                    do_step_new(metal_dict_temp, next_step)
+                    continue
+
+            elif (sign == "<" and metal_dict[xmas][0] < int(target)):
+                metal_dict_temp = metal_dict.copy()
+                metal_dict_temp[xmas] = (metal_dict[xmas][0], int(target)-1)
+                metal_dict[xmas] = (int(target), metal_dict[xmas][1])
+                if next_step == "A":
+                    accept_bin.append(metal_dict_temp)
+                    print("==Part accepted==")
+                    continue
+                elif next_step == "R":
+                    reject_bin.append(metal_dict_temp)                    
+                    print("==Part rejected==")
+                    continue
+                else:
+                    do_step_new(metal_dict_temp, next_step)
+                    continue
             else:
                 continue
 
@@ -91,4 +151,22 @@ if __name__ == "__main__":
 
     print(sum([sum(i.values()) for i in accept_bin]))
 
-
+    # part two starts here
+    accept_bin = []
+    reject_bin = []
+    new_metal_dict = {'x': (1, 4000), 'm': (1, 4000), 'a': (1, 4000), 's': (1, 4000)}
+    stack = []
+    do_step_new(new_metal_dict, "in")
+    print("==Accept bin==")
+    print(accept_bin)
+    print("==Reject bin==")
+    print(reject_bin)
+    total = 0
+    for item in accept_bin:
+        subtotal = 1
+        for char in item:
+            subtotal *= (item[char][1] - item[char][0] + 1)
+        total += subtotal
+    print("==Total==")
+    print(total)
+    # in{m>2048:npv,qmn}
